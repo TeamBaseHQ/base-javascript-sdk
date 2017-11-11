ec42fe5edfd84c77cd6f2812747382c3441c5848
Kunal Varma <kunalvarma05@gmail.com>
2017-11-12 05:06:49 +0530

    Base Model

---
+++ src/models/BaseModel.ts
@@ -0,0 +1,49 @@
+class BaseModel {
+  /**
+   * Model id
+   */
+  id: number;
+
+  /**
+   * Model created_at
+   */
+  created_at: string;
+
+  /**
+   * Model updated_at
+   */
+  updated_at: string;
+
+  constructor() {
+    this.id = 0;
+    this.created_at = null;
+    this.updated_at = null;
+  }
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public setId(id: number): BaseModel {
+    this.id = id;
+    return this;
+  }
+
+  public getCreated_at(): string {
+    return this.created_at;
+  }
+
+  public setCreated_at(created_at: string): BaseModel {
+    this.created_at = created_at;
+    return this;
+  }
+
+  public getUpdated_at(): string {
+    return this.updated_at;
+  }
+
+  public setUpdated_at(updated_at: string): BaseModel {
+    this.updated_at = updated_at;
+    return this;
+  }
+}
