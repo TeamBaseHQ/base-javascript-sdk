6008da7ac5308fb6f8c403f0f70d11e0a9c0a67c
Kunal Varma <kunalvarma05@gmail.com>
2017-11-19 04:03:03 +0530

    Add makeChannelFromResponse and makeChannel method to ChannelService

--- src/services/ChannelService.ts
+++ src/services/ChannelService.ts
@@ -5,4 +5,17 @@ export default class ChannelService {
   constructor(public base: Base) {
     //
   }
+
+  static makeChannelFromResponse(response: any): Channel {
+    const data = response.data ? response.data.data : {};
+    return ChannelService.makeChannel(data);
+  }
+
+  static makeChannel(data: any): Channel {
+    return (new Channel())
+      .setName(data.name)
+      .setDescription(data.description)
+      .setSlug(data.slug)
+      .setUser_id(data.user_id);
+  }
 }
