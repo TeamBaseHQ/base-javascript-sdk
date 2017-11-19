45827a245ee4a70d9d9645f4afddac6cb80bd6c2
Kunal Varma <kunalvarma05@gmail.com>
2017-11-20 02:31:31 +0530

    Add factory to BaseCollection

--- dist/module/models/BaseCollection.d.ts
+++ dist/module/models/BaseCollection.d.ts
@@ -2,13 +2,52 @@ export default class BaseCollection<T> {
     data: T[];
     links: object;
     meta: object;
-    constructor(data: T[], links?: object, meta?: object);
+    constructor(responseData: any, type: {
+        new (tData: any): T;
+    });
+    /**
+     * Set data.
+     *
+     * @param {T[]} data
+     * @return {BaseCollection<T>}
+     */
     setData(data: T[]): BaseCollection<T>;
+    /**
+     * Get data.
+     * @return {T[]}
+     */
     getData(): T[];
+    /**
+     * Set links.
+     *
+     * @param {Object} links
+     * @return {BaseCollection<T>}
+     */
     setLinks(links: object): BaseCollection<T>;
+    /**
+     * Get links.
+     * @return {Object}
+     */
     getLinks(): object;
+    /**
+     * Set meta.
+     *
+     * @param {Object} meta
+     * @return {BaseCollection<T>}
+     */
     setMeta(meta: object): BaseCollection<T>;
+    /**
+     * Get meta.
+     * @return {Object}
+     */
     getMeta(): object;
+    /**
+     * Model Factory.
+     *
+     * @param type
+     * @param data
+     * @return {T}
+     */
     static factory<T>(type: {
         new (tData: any): T;
     }, data: any): T;
--- src/models/BaseCollection.ts
+++ src/models/BaseCollection.ts
@@ -1,36 +1,88 @@
-export default class BaseCollection<T> {
+import BaseModel from './BaseModel';
 
-  constructor(public data: T[], public links?: object, public meta?: object) {
-    //
+export default class BaseCollection<T> {
+  data: T[];
+
+  links: object;
+
+  meta: object;
+
+  constructor(responseData: any, type: { new (tData: any): T }) {
+    const data = responseData.data ? responseData.data : {};
+
+    const collectionData = data.data.map((itemData: any) => {
+      return BaseCollection.factory(type, itemData);
+    });
+
+    this.setData(collectionData)
+      .setLinks(data.links)
+      .setMeta(data.meta);
   }
 
+  /**
+   * Set data.
+   *
+   * @param {T[]} data
+   * @return {BaseCollection<T>}
+   */
   public setData(data: T[]): BaseCollection<T> {
     this.data = data;
     return this;
   }
 
+  /**
+   * Get data.
+   * @return {T[]}
+   */
   public getData(): T[] {
     return this.data;
   }
 
+  /**
+   * Set links.
+   *
+   * @param {Object} links
+   * @return {BaseCollection<T>}
+   */
   public setLinks(links: object): BaseCollection<T> {
     this.links = links;
     return this;
   }
 
+  /**
+   * Get links.
+   * @return {Object}
+   */
   public getLinks(): object {
     return this.links;
   }
 
+  /**
+   * Set meta.
+   *
+   * @param {Object} meta
+   * @return {BaseCollection<T>}
+   */
   public setMeta(meta: object): BaseCollection<T> {
     this.meta = meta;
     return this;
   }
 
+  /**
+   * Get meta.
+   * @return {Object}
+   */
   public getMeta(): object {
     return this.meta;
   }
 
+  /**
+   * Model Factory.
+   *
+   * @param type
+   * @param data
+   * @return {T}
+   */
   static factory<T>(type: { new (tData: any): T }, data: any): T {
     return new type(data);
   }
