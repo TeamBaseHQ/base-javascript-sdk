0174701123d626020015b7fa942fec5be56958a4
Kunal Varma <kunalvarma05@gmail.com>
2017-11-20 03:02:30 +0530

    Add getChannelMember to ChannelMemberService

--- src/services/ChannelMemberService.ts
+++ src/services/ChannelMemberService.ts
@@ -6,4 +6,19 @@ export default class ChannelMemberService {
   constructor(public base: Base) {
     //
   }
+
+  /**
+   * Get Team Member.
+   *
+   * @param team
+   * @param {string} slug
+   * @param {string} user_id
+   * @return {Promise<User>}
+   */
+  getChannelMember(team: string, slug: string, user_id: string): Promise<User> {
+    return this.base.get(`/teams/${team}/channels/${slug}/members/${user_id}`)
+      .then((response) => {
+        return new User(response.data.data);
+      });
+  }
 }
