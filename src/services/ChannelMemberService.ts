29cafbf8ea69e4fc72f8531977c11d4be0473766
Kunal Varma <kunalvarma05@gmail.com>
2017-11-20 03:01:52 +0530

    ChannelMemberService

---
+++ src/services/ChannelMemberService.ts
@@ -0,0 +1,9 @@
+import Base from '../Base';
+import User from '../models/User';
+import BaseCollection from '../models/BaseCollection';
+
+export default class ChannelMemberService {
+  constructor(public base: Base) {
+    //
+  }
+}
